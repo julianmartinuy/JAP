d645f6cc53de628bc18502dfe45ce71b73790021
julianmartinuy <110436445+julianmartinuy@users.noreply.github.com>
2022-09-09 11:27:43 -0300

    Entregable 1 Definitivo

--- workspace-inicial/js/login.js
+++ workspace-inicial/js/login.js
@@ -2,36 +2,53 @@ function validation ()
 {
     var form =document.getElementById("form");
     var email =document.getElementById("email").value;
+    var pass =document.getElementById("pass").value;
     var text= document.getElementById("text");
+    var textpass= document.getElementById("textpass");
     var pattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var patternpass=/^[^ ]/;
     if (email.match(pattern))
     {
         form.classList.add("valid");
         form.classList.remove("invalid");
-        text.innerHTML="El correo electronico ingresado es valido";
+        text.innerHTML="El correo electrónico ingresado es valido";
         text.style.color="#008000";
         
 
 
-        text.style.color="#00ff00";
-        window.location.href="index.html";
-
+        
     }
     else
     {
         form.classList.remove("valid");
         form.classList.add("invalid");
-        text.innerHTML="El correo electronico ingresado NO es valido";
+        text.innerHTML="El correo electrónico ingresado NO es valido";
         text.style.color="#FF0000";
         
     }
-    if (email== "")
+    if (email.match(pattern) && pass.match(patternpass)){
+        
+        setTimeout(function () {
+        window.location.href = "index.html";
+          }, 1500);
+    }
+    if (email== "" )
     {
         form.classList.remove("valid");
         form.classList.remove("invalid");
-        text.innerHTML="";
-        text.style.color="#00ff00";
+        text.innerHTML="Ingresa Correo Electrónico";
+        text.style.color="#FF0000";
     }
+    if (pass== "" )
+    {
+        form.classList.remove("valid");
+        form.classList.remove("invalid");
+        textpass.innerHTML="Ingresa Contraseña";
+        textpass.style.color="#FF0000";
+    }
+    
+    
+
 
 }
 
